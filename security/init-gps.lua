17ab9f2a414e10d60b343a32e5957f1a86325e66
Yugo l'Eliatrop <papasergiusv@gmail.com>
2026-05-20 14:55:30 +0200

    simplify gps

--- security/init-gps.lua
+++ security/init-gps.lua
@@ -1,12 +1,1 @@
-local x, y, z = 23, 65, -33
-
-local modem = peripheral.find("modem")
-if not modem then
-  print("No modem found!")
-  return
-end
-
-modem.open(gps.CHANNEL_GPS)
-
-print("Starting GPS tower at coordinates: " .. x .. ", " .. y .. ", " .. z)
-gps.host(x, y, z)
+shell.run("gps", "host", 0, 0, 0)
